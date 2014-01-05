3e9b51674c780ac7a46a68fe762ef3dff57fc162
黃文龍 <wlhunag@gmail.com>
2014-01-05 12:53:34 +0800

    add batch file

---
+++ setup.bat
@@ -0,0 +1,19 @@
+@echo off 
+rmdir build /s /q
+pause
+setup_cxfreeze.py build
+
+@echo 接下來要壓縮亂數程式
+pause
+
+"C:\Program Files\7-Zip\7z.exe" a -tzip build\抽抽樂-自訂名單.zip build\exe.win32-2.7
+@echo 接下來要製作安裝檔案
+
+@echo
+rem "C:\Program Files (x86)\Inno Setup 5\Compil32.exe" /cc "C:\Users\Aaron\Documents\random_choice_portable_Ramdisk.iss"
+
+@echo
+@echo 接下來要連線到 ftp
+pause
+
+"C:\Program Files (x86)\FileZilla FTP Client\filezilla.exe" --site="0D99 manager"
